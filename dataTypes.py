84f5d52f7619760071bc43658b189924c72260c2
anukulghoshdev <anukulghosh264@gmail.com>
2024-11-05 15:59:54 +0600

    datatype details

--- dataTypes.py
+++ dataTypes.py
@@ -1,2 +1,13 @@
 # str, int, float, complex, list, tuple, dict, set, range, bool, None
 # frozenset, bytes, bytearray, memoryview
+a = "anukul" #str
+b = 23 #int
+c = 32.4 #float
+d = 11j #coplex
+e = ["volvo", "saab", "toyota"] #list
+f = ("volvo", "saab", "toyota") #tuple
+h = {"volvo", "saab", "toyota"} #set
+g = {"name": "John", "age":23} #set
+h = range(6)
+i = True #bool
+j = None #NoneType
